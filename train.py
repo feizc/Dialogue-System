f8ff739a3451b3e1573d62fc730eeeab1dce7cc7
zcfei <3300949577@qq.com>
2020-12-04 14:51:00 +0800

    process

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 from transformers import GPT2Config, BertTokenizer
 import os
 import sys
-from model import MMdialog 
+from MMdialog import MMdialog 
 import torch 
 
 
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     
     config = GPT2Config(vocab_size=13088)
     print(config)
-    tokenizer = BertTokenizer.from_pretrained('./huggingface_ckpt/cdial-gpt', do_lower_case=True)
+    tokenizer = BertTokenizer.from_pretrained('./ckpt/cdial-gpt', do_lower_case=True)
     input = '你好啊'
     input = tokenizer.tokenize(input)
     input = tokenizer.convert_tokens_to_ids(input)
